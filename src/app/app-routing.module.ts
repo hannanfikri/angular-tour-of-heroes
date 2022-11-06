d67d4a6cae141b8435efbc62d5874019b9025c6c
Hannan Fikri <hannanfikri510@gmail.com>
2022-11-06 11:16:14 +0800

    Tutorial of angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent},
+  { path: 'dashboard', component: DashboardComponent},
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  { path: 'detail/:id', component: HeroDetailComponent}
+]; //routes array
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)], //add RouterModule to the AppRoutingModule
+  exports: [RouterModule] // to make RouterModule available throughout the app
 })
 export class AppRoutingModule { }
